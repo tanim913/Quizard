033ca2e2f22cf1f563a29daf6eee4f5cf0401c31
Tanim <42853370+tanim913@users.noreply.github.com>
2021-03-02 14:56:26 +0600

    adding files to this repository

---
+++ Quizard/src/Player.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author tanim
+ */
+public class Player {
+    private String name;
+    private int score;
+
+    public Player() {
+    }
+
+    public Player(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+    
+    
+}
